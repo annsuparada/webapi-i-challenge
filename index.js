8e7a4fe44ff32bc1c0c5efacb4eec2b7e598c89a
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-02 15:55:21 -0500

    GET /api/users

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// implement your API here
+const express = require('express');
+const Data = require('./data/db.js');
+const server = express();
+server.use(express.json());
+
+//POST
+
+//GET
+server.get('/api/users', (req, res) => {
+    Data.find()
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'error getting the list of user'})
+    })
+})
+
+
+//GET
+
+//DELETE
+
+//PUT
+
+
+const port = 8000;
+server.listen(port, () => console.log('api running'))
